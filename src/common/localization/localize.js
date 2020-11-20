bc033ca0f65810582b46763c36acb0f1bf5ea076
Son of Ishtar <71618853+SonOfIshtar@users.noreply.github.com>
2020-11-20 22:25:49 +1300

    [feature] Add support to Russian translation + other translation fixes (#84)
    * Update and rename en-US.json to ru-RU.json
    
    * [bugfix] Pu back US translation file
    
    * [bugfix] Correct Chinese language names display
    
    * [bugfix] Add translation forgotten for order form price field
    
    * [feature] Integrate russian translation
    
    Co-authored-by: SHA-2048 <8516712+SHA-2048@users.noreply.github.com>

--- src/common/localization/localize.js
+++ src/common/localization/localize.js
@@ -5,20 +5,23 @@ import en from './translations/en-US.json'
 import fr from './translations/fr-FR.json'
 import cn from './translations/zh-CN.json'
 import tw from './translations/zh-TW.json'
+import ru from './translations/ru-RU.json'
 
 export let supportedLanguages = {
     'en-US': en,
     'fr-FR': fr,
     'zh-CN': cn,
-    'zh-TW': tw
+    'zh-TW': tw,
+    'ru-RU': ru,
 }
 
 export async function initLocalizations() {
     return i18next
         .use(LanguageDetector)
         .init({
-            fallbackLng: 'en',
+            fallbackLng: 'en-US',
             debug: false,
+            supportedLngs: Object.keys(supportedLanguages),
             resources: supportedLanguages
         }, function(err, t) {})
 }

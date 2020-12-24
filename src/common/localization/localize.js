a52d61cecb324f5d4e208d3398574d12879c3a67
SHA_2048 <8516712+SHA-2048@users.noreply.github.com>
2020-12-24 05:10:44 +0100

    [ux] Optimize order from interactions + layout changes (#105)

--- src/common/localization/localize.js
+++ src/common/localization/localize.js
@@ -1,18 +1,33 @@
 import i18next from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import { Russian } from "flatpickr/dist/l10n/ru.js"
+import { French } from "flatpickr/dist/l10n/fr.js"
+import { Mandarin } from "flatpickr/dist/l10n/zh.js"
+import { MandarinTraditional } from "flatpickr/dist/l10n/zh-tw.js"
+import { english } from "flatpickr/dist/l10n/default.js"
+
 import en from './translations/en-US.json'
 import fr from './translations/fr-FR.json'
 import cn from './translations/zh-CN.json'
 import tw from './translations/zh-TW.json'
 import ru from './translations/ru-RU.json'
 
+
+export let supportedDateTranslations = {
+    'en-US': english,
+    'fr-FR': French,
+    'zh-CN': Mandarin,
+    'zh-TW': MandarinTraditional,
+    'ru-RU': Russian
+}
+
 export let supportedLanguages = {
     'en-US': en,
     'fr-FR': fr,
     'zh-CN': cn,
     'zh-TW': tw,
-    'ru-RU': ru,
+    'ru-RU': ru
 }
 
 export async function initLocalizations() {

82ce358afce3d9062191cc83a6e7b2da115746e9
SHA_2048 <8516712+SHA-2048@users.noreply.github.com>
2020-12-31 11:02:55 +0100

    [ux] Re-work orderbook
    Always keep red/sell at the top section
    and green/buy at the bottom adjusting
    colors on the appropriate columns as
    per price switch selection

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,21 +1,31 @@
 import numeral from "numeral";
 
-export function formatNumber(n, precision = 3) {
+export function formatNumber(n, precision = 3, fixedWidth = false) {
     if (n === 0 || n > (1 / (10 ** precision))) {
-        return numeral(n).format(precisionToFormat(precision))
+        return numeral(n).format(precisionToFormat(precision, fixedWidth))
     } else {
-        return numeral(n).format(`${precisionToFormat(precision)}e+0`)
+        return numeral(n).format(`${precisionToFormat(precision, fixedWidth)}e+0`)
     }
 }
 
-function precisionToFormat(precision) {
-    let format = precision > 0 ? "0,0.[" : "0,0"
-    for(let i = 0; i < precision; i++) {
-        format += "0"
-    }
+function precisionToFormat(precision, fixedWidth) {
+    let format = "0,0"
 
-    if ( precision > 0 ) {
-        format += "]"
+    if (precision > 0) {
+        if (fixedWidth) {
+            format = "0,0."
+        } else {
+            format = "0,0.["
+        }
+
+        for(let i = 0; i < precision; i++) {
+            format += "0"
+        }
+
+        if (!fixedWidth) {
+            format += "]"
+        }
+
     }
 
     return `${format}`

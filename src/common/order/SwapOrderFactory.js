8aaf74aae28da6c4681e990f8b94cad21b7adf81
SHA_2048 <8516712+SHA-2048@users.noreply.github.com>
2021-02-13 22:15:20 +0100

    [logging] distinguish Wrap, UnWrap and Swap transactions

--- src/common/order/SwapOrderFactory.js
+++ src/common/order/SwapOrderFactory.js
@@ -1,6 +1,6 @@
 import {OrderFactory} from "./OrderFactory";
 import {callSwapApi} from "../0x/0x_swap_proxy";
-import {ExchangeProxyAllowanceTargetAddress} from "../tokens/token_fetch";
+import {ExchangeProxyAllowanceTargetAddress, tokensList} from "../tokens/token_fetch";
 import {OrderState} from "./OrderStateManager";
 import {formatNumber} from "../helpers";
 import {BigNumber} from "@0x/utils";
@@ -27,6 +27,9 @@ export class SwapOrderFactory extends OrderFactory {
 
     async sendOrder(order) {
 
+        let sellToken = tokensList().find(t => t.address.toLowerCase() === order.sellToken)
+        let buyToken = tokensList().find(t => t.address.toLowerCase() === order.buyToken)
+
         let quote = await callSwapApi(order)
 
         quote.from = this.account
@@ -34,17 +37,23 @@ export class SwapOrderFactory extends OrderFactory {
         await this.stateManager.setInProgressState(
             OrderState.SWAPPING,
             {
-                sellAmount: formatNumber(quote.sellAmount / (10 ** this.order.sellToken.decimals)),
-                sellToken: this.order.sellToken,
-                buyAmount: formatNumber(quote.buyAmount / (10 ** this.order.buyToken.decimals)),
-                buyToken: this.order.buyToken
+                sellAmount: formatNumber(quote.sellAmount / (10 ** sellToken.decimals)),
+                sellToken: order.sellToken,
+                buyAmount: formatNumber(quote.buyAmount / (10 ** buyToken.decimals)),
+                buyToken: order.buyToken
             },
             true
         )
 
         await window.web3Modal.eth.sendTransaction(quote);
 
-        Rollbar.info("Swap succeeded")
+        if (sellToken.symbol === "ETH" && buyToken.symbol === "WETH") {
+            Rollbar.info("Wrap succeeded")
+        } else if (sellToken.symbol === "ETH" && buyToken.symbol === "WETH") {
+            Rollbar.info("UnWrap succeeded")
+        } else {
+            Rollbar.info("Swap succeeded")
+        }
     }
 
     buildOrderDetails(sellAmount, buyAmount) {

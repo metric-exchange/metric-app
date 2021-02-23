a439d74fa7e5ed86b3b0420d3576e2b28a686c99
SHA-2084 <2048.sha@gmail.com>
2021-02-23 23:01:23 +0100

    [chore] Update dependencies

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import "@babel/polyfill";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import '@riotjs/hot-reload'
 import { component, install } from 'riot'
 import jquery from "jquery";

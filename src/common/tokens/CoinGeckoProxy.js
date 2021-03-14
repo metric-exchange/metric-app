3032500bc42d70abe8809c410ed9c06e746f48e0
SHA-2084 <2048.sha@gmail.com>
2021-03-14 19:46:02 +0100

    [bugfix] Fix ethereum price fetch using coinGecko api

--- src/common/tokens/CoinGeckoProxy.js
+++ src/common/tokens/CoinGeckoProxy.js
@@ -1,11 +1,17 @@
 import {fetchJson} from "../json_api_fetch";
+import {tokensList} from "./token_fetch";
 
 export class CoinGeckoProxy {
 
     constructor() {}
 
     async fetchCoinPriceAt(tokenAddress, date) {
-        let coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum/contract/${tokenAddress.toLowerCase()}`)
+        let coin = undefined
+        if (tokenAddress === tokensList().find(t => t.symbol.toLowerCase() === "eth").address) {
+            coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum`)
+        } else {
+            coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum/contract/${tokenAddress.toLowerCase()}`)
+        }
         if (coin && coin.id) {
             let formattedDate = date.format("DD-MM-YYYY")
             let coinInfo =

20091c93969bd247fab475c89baa9b4b3ac97efb
SHA-2084 <2048.sha@gmail.com>
2021-03-17 07:35:25 +0100

    [bugfix] Catch failing calls to token price

--- src/common/tokens/CoinGeckoProxy.js
+++ src/common/tokens/CoinGeckoProxy.js
@@ -6,22 +6,25 @@ export class CoinGeckoProxy {
     constructor() {}
 
     async fetchCoinPriceAt(tokenAddress, date) {
-        let coin = undefined
-        if (tokenAddress === tokensList().find(t => t.symbol.toLowerCase() === "eth").address) {
-            coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum`)
-        } else {
-            coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum/contract/${tokenAddress.toLowerCase()}`)
-        }
-        if (coin && coin.id) {
-            let formattedDate = date.format("DD-MM-YYYY")
-            let coinInfo =
-                await fetchJson(`https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=${formattedDate}&localization=false`)
-
-            if (coinInfo.market_data && coinInfo.market_data.current_price) {
-                return coinInfo.market_data.current_price.usd
+        try {
+            let coin = undefined
+            if (tokenAddress === tokensList().find(t => t.symbol.toLowerCase() === "eth").address) {
+                coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum`)
+            } else {
+                coin = await fetchJson(`https://api.coingecko.com/api/v3/coins/ethereum/contract/${tokenAddress.toLowerCase()}`)
             }
-        }
+            if (coin && coin.id) {
+                let formattedDate = date.format("DD-MM-YYYY")
+                let coinInfo =
+                    await fetchJson(`https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=${formattedDate}&localization=false`)
 
+                if (coinInfo.market_data && coinInfo.market_data.current_price) {
+                    return coinInfo.market_data.current_price.usd
+                }
+            }
+        } catch (e) {
+            console.warn("Could not fetch token price")
+        }
 
         return undefined
     }

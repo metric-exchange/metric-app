ae1902a604411eec4098378bde86958731b9e028
SHA-2084 <2048.sha@gmail.com>
2021-03-19 22:18:41 +0100

    [feature] default fee to 0.1% for non ethereum chains

--- src/common/metric_fee.js
+++ src/common/metric_fee.js
@@ -1,9 +1,14 @@
 import {tokensList} from "./tokens/token_fetch";
+import {isConnectedToEthereumMainNet} from "./ChainHelpers";
 
 export let MetricReferralAddress = "0x52427b0035F494a21a0A4A1AbE04d679f789c821"
 
 export function calculateMetricFee() {
-    return fetchMetricBalance() >= 200 ? 0.001 : 0.003
+    if (isConnectedToEthereumMainNet()) {
+        return fetchMetricBalance() >= 200 ? 0.001 : 0.003
+    }
+
+    return 0.001
 }
 
 export function fetchMetricBalance() {

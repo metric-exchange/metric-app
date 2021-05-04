c88132fd2b61fdc634786bf30f44413e3bf6ff66
SHA-2084 <2048.sha@gmail.com>
2021-05-04 12:22:23 +0200

    [ux] Re-arrange balance display and fix format

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,6 +1,10 @@
 import numeral from "numeral";
 
 export function formatNumber(n, precision = 3, fixedWidth = false) {
+    if (isNaN(n)) {
+        return '-'
+    }
+
     if (n === 0 || n > (1 / (10 ** precision))) {
         return numeral(n).format(precisionToFormat(precision, fixedWidth))
     } else {
